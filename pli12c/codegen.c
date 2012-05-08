6d53eb75ea66880345d04230c13a1b0f0f376773
Clement Poh <cpoh@unimelb.edu.au>
2012-05-09 00:09:24 +1000

    Doing codegen very slowly, now pushes the stack.

--- pli12c/codegen.c
+++ pli12c/codegen.c
@@ -8,22 +8,25 @@
 #include    "ast.h"
 #include    "t12.h"
 #include    "codegen.h"
+#include    "pli12c.h"
 
-make_call(char *func);
-make_label(char *str);
+Instr make_instr(Opcode op);
+Instr make_instr_str(Opcode op, char *str, const char *pre);
+
 Code translate_func(Func f);
 
 Code
 translate_prog(Funcs prog)
 {
-    Instr call = { .op = OP_CALL, .string_const = "main" };
-    Instr halt = { .op = OP_HALT };
+    Instr halt = make_instr(OP_HALT);
+    Instr call = make_instr_str(OP_CALL, "main", "func");
 
-    Code code = instr_to_code(call);
-    code = seq(translate_func(halt), code);
+    Code code = instr_to_code(halt);
+    code = seq(instr_to_code(call), code);
+    
     while(prog) {
-        code = seq(translate_func(prog->f_first), code);
-        prog = prog-f_rest;
+        code = seq(code, translate_func(prog->f_first));
+        prog = prog->f_rest;
     }
 
     return code;
@@ -31,28 +34,44 @@ translate_prog(Funcs prog)
 
 Code
 translate_func(Func f) {
-    Instr lbl = make_label(f->id);
-    
-    
-    
-    
+    Decls decls = f->decls;
+    int locals = 0;
 
+    Instr instr = make_instr_str(OP_LABEL, f->id, "func");
+    Code code = instr_to_code(instr);
+
+    while(decls) {
+        locals++;
+        instr = make_instr_str(OP_COMMENT, "variable is %s in stack slot %d");
+        decls = decls->d_rest;
+    }
+
+    instr = make_instr(OP_PUSH_STACK_FRAME);
+    instr->int_const = locals;
+    code = seq(code, instr_to_code(instr));
+
+    
+    return code;
 }
 
-make_call(char *func) {
+Instr make_instr(Opcode op) {
     Instr new = checked_malloc(sizeof(*new));
 
-    new->opcode = OP_CALL;
-    new->string_const = "main";
+    new->opcode = op;
 
     return new;
 }
 
-make_label(char *str) {
-    Instr new = checked_malloc(sizeof(*new));
+Instr make_instr_str(Opcode op, char *str, const char *pre) {
+    Instr new = make_instr(op);
+    char buffer[128];
 
-    new->opcode = OP_LABEL;
-    new->string_const = str;
-
+    if (pre) {
+        sprintf(buffer, "%s_%s", pre, str);
+    } else {
+        sprintf(buffer, "%s", str);
+    }
+    
+    new->string_const = checked_strdup(buffer);
     return new;
 }
